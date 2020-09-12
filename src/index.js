8c88d74a1a462a30bda27ef82d6872f6157ac9e0
jnghg <eniows@naver.com>
2020-09-12 21:28:39 +0900

    react express 연동

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Routes from './routes';
-import stores from './stores';
 
-ReactDOM.render(<Routes {...stores}/>, document.getElementById('root'));
+ReactDOM.render(<Routes />, document.getElementById('root'));
--- src/instarClone/stores/InstarStore.js
+++ src/instarClone/stores/InstarStore.js
@@ -1,6 +1,33 @@
+import { observable, action, toJS, computed, runInAction, instance } from 'mobx';
+import { autobind } from 'core-decorators';
+import InstarRepository from '../repository/InstarRepository';
 
+@autobind
 class InstarStore {
-    
+
+    @observable
+    user = {
+        name: '',
+        age: '',
+    };
+
+    // api 호출
+    @action
+    async callSayHello() {
+
+        const result = await InstarRepository.callSayHello();
+
+        console.log('return :', result);
+
+        runInAction(() => {
+            this.user = {
+                ...this.user,
+                name: result.name || '',
+                age: result.age || '',
+            };
+        });
+    }
+
 }
 
-export default InstarStore;
+export default new InstarStore();
